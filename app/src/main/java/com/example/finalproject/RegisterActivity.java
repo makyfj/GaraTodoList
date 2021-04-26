9c2ae103d85d33146369c1fb84c42995424d945e
Maky <franklinjaradev@gmail.com>
2021-04-25 20:35:42 -0400

    Project done

--- app/src/main/java/com/example/finalproject/RegisterActivity.java
+++ app/src/main/java/com/example/finalproject/RegisterActivity.java
@@ -2,7 +2,16 @@ package com.example.finalproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -74,6 +83,5 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
-
     }
 }
